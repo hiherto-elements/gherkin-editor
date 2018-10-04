874f2a1a25ef4356a765c5b09750a30fb2a5b33e
Sebastian Schürmann <sebs@2xs.org>
2018-10-04 17:22:14 +0200

    Updates

--- src/_element/_element.js
+++ src/_element/_element.js
@@ -1,30 +1,8 @@
-import { html, PolymerElement } from "../../node_modules/@polymer/polymer/polymer-element.js";
-/**
- * @customElement
- * @polymer
- */
-
-class GherkinEditorApp extends PolymerElement {
-  static get template() {
-    return html`
+import{html,PolymerElement}from"../../node_modules/@polymer/polymer/polymer-element.js";class GherkinEditorApp extends PolymerElement{static get template(){return html`
       <style>
         :host {
           display: block;
         }
       </style>
       <h2>Hello [[prop1]]!</h2>
-    `;
-  }
-
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'gherkin-editor-app'
-      }
-    };
-  }
-
-}
-
-window.customElements.define('gherkin-editor-app', GherkinEditorApp);
+    `}static get properties(){return{prop1:{type:String,value:"gherkin-editor-app"}}}}window.customElements.define("gherkin-editor-app",GherkinEditorApp);
