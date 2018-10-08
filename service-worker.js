4a36067aa9f0cbaae8c1c03a29fcd2590ea38fbd
Sebastian Schürmann <sebs@2xs.org>
2018-10-08 23:45:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","8e3a2300cb334eb9561008e2dc2fc1a1"]];
+var precacheConfig = [["index.html","2a10a8ec2ee073c082c320f078ab56e1"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
